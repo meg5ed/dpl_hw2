40dba2770546388389f3138d3e1574d95b48bbce
“Meg <meg5ed@virginia.edu>
2021-08-26 20:22:30 -0600

    update casino

--- casino.rb
+++ casino.rb
@@ -1,6 +1,16 @@
 # Start game player has a name and an initial bankroll
 # Player can go to different games via menu
+require_relative './Player'
+
 # Slots
 # High / Low
 # Player places bet and wins / loses (hint: rand)
 # Player's bankroll goes up and down with wins and losses
+
+
+#deck card
+
+def main_menu
+  player = Player.new
+
+end
